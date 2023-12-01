5afc43bd273f5b63e530db330065ccc3208d3d0e
JolanEggers <126364107+JolanEggers@users.noreply.github.com>
2023-12-01 13:17:56 +0100

    Refactored

--- 2.Vortrag/PullStockData.py
+++ 2.Vortrag/PullStockData.py
@@ -1,34 +1,22 @@
-import random
 import numpy as np
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 from scipy.stats import skew
 import yfinance as yf
 import pandas as pd
 
-# print all columns
-pd.set_option("display.max_columns", None)
-
-# set collumns width to 100
-pd.set_option("display.width", None)
-
 
 def pullStockData(symbol, start_date, end_date):
+    pd.set_option("display.max_columns", None) # print all columns
+    pd.set_option("display.width", None) # set collumns width to 100
     ticker = yf.Ticker(symbol)
     stock_data = ticker.history(start=start_date, end=end_date)
-
     data_list = stock_data["Close"]
-    stock_data_today = data_list[-1]
     prices = []
     for i in range(len(data_list)):
         prices.append(data_list[i])
 
     return prices
 
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import skew
 
 def markovAnalysis(prices, simDauer, anzahlSims, symbol):
     pricesX = []
@@ -49,7 +37,6 @@ def markovAnalysis(prices, simDauer, anzahlSims, symbol):
             r = np.exp(Zufallszahl * std / mean * np.sqrt(1.0 / len(prices)))
             y[n].append(y[n][t] * r)
 
-    # Create a subplot with 2 rows and 1 column
     plt.subplot(2, 1, 1)
     plt.plot(pricesX, prices)
     for n in range(anzahlSims):
@@ -61,14 +48,14 @@ def markovAnalysis(prices, simDauer, anzahlSims, symbol):
     plt.xlabel("Zeit in Tagen")
     plt.ylabel("Preis in €")
 
-    # Create a subplot with 2 rows and 1 column
     plt.subplot(2, 1, 2)
     distributions = []
     for i in range(anzahlSims):
         distributions.append(y[i][100])
     plt.hist(distributions, bins=30, density=True, alpha=0.7, color='skyblue', edgecolor='black')
 
-    plt.title('Product Distribution, skew = '+str(int(1000*skew(distributions))/1000.0), fontsize=16, fontweight='bold')
+    plt.title('Product Distribution, skew = ' + str(int(1000 * skew(distributions)) / 1000.0), fontsize=16,
+              fontweight='bold')
     plt.xlabel('Value', fontsize=12)
     plt.ylabel('Probability Density', fontsize=12)
     plt.grid(axis='y', linestyle='--', alpha=0.7)
@@ -82,7 +69,7 @@ def markovAnalysis(prices, simDauer, anzahlSims, symbol):
     plt.show()
 
 
-#symbol = "RHM.DE"
-symbol = "AAPL"
+symbol = "RHM.DE"
+# symbol = "AAPL"
 prices = pullStockData(symbol, "2022-11-27", "2023-11-27")
 markovAnalysis(prices, 100, 500, symbol)
